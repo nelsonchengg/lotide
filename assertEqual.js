7d9667185325c8c314a0b3a745ff33764a6bff18
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-06 14:08:21 -0500

    New and Improved

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   let statement = "";
   if (actual === expected) {
-    statement = "Assertion Passed: " + actual + " === " + expected;
+    statement = `Assertion Passed:  ${actual} === ${expected}`;
   }
   if (actual !== expected) {
-    statement = "Assertion Failed: " + actual + " !== " + expected;
+    statement = `Assertion Failed:  ${actual} !== ${expected}`;
   }
   return statement;
 };
