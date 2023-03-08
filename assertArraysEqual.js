fb264916ea0d96ee2a652e87f4bac12e40fb4134
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-07 23:55:37 -0500

    created a function to compare arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    let set1 = array1[i];
+    let set2 = array2[i];
+    if (set1 !== set2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    return `Assertion Passed: ${array1} === ${array2}`;
+  }
+  if (eqArrays(array1, array2) === false) {
+    return `Assertion Failed: ${array1} !== ${array2}`;
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
--- eqArrays.js
+++ eqArrays.js
@@ -10,6 +10,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     let set1 = array1[i];
     let set2 = array2[i];
