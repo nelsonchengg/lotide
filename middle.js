a47bcd13f973b4d8459e1707d5d414a5f7f1da4a
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-08 16:13:56 -0500

    created a middle function

---
+++ middle.js
@@ -0,0 +1,48 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    let set1 = array1[i];
+    let set2 = array2[i];
+    if (set1 !== set2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    return `Assertion Passed: ${array1} === ${array2}`;
+  }
+  if (eqArrays(array1, array2) === false) {
+    return `Assertion Failed: ${array1} !== ${array2}`;
+  }
+};
+
+const middle = function(numbers) {
+  const result = []
+  const length = numbers.length
+  if (length <= 2) {
+    return result;
+  }
+  if (length % 2 === 0) {
+    let index1 = (length / 2) - 1;
+    let index2 = length / 2;
+    result.push(numbers[index1], numbers[index2]);
+  }
+  if (length % 2 !== 0) {
+    let index = Math.floor(length / 2);
+    result.push(numbers[index]);
+  }
+  return result;
+};
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([2, 7, 9, 7, 4, 6, 7 , 9])); // = > [7, 4]
