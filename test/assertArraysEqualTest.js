2d87184476f73cde4ca2d57e1aaae2f332d0d75d
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-13 21:02:14 -0400

    Refactor assertArrayEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../eqArrays')
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays(assertArraysEqual([1, 2, 3], [1, 2, 3]), true));
