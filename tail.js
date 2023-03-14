d7a24ff8db12a5e951a9450586b78311e2238523
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-13 21:53:06 -0400

    refactor tail

--- tail.js
+++ tail.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let statement = "";
-  if (actual === expected) {
-    statement = `Assertion Passed:  ${actual} === ${expected}`;
-  }
-  if (actual !== expected) {
-    statement = `Assertion Failed:  ${actual} !== ${expected}`;
-  }
-  return statement;
-};
-
 const tail = function(array) {
   let value = "";
   value = array.slice(1);
