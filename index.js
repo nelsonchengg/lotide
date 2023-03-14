2aa3f597f3b37a3642f16e3f67f15703d7955a31
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-13 23:42:15 -0400

    shorthand index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
@@ -7,7 +7,7 @@ const letterPositions = require('./letterPositions');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
-const findKey = require('./findKey')
+const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const eqObjects = require('./eqObjects');
 const eqArrays = require('./eqArrays');
@@ -17,20 +17,20 @@ const assertObjectsEqual = require('./assertObjectsEqual');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  assertObjectsEqual: assertObjectsEqual
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  findKey,
+  findKeyByValue,
+  eqArrays,
+  eqObjects,
+  countLetters,
+  countOnly,
+  assertObjectsEqual
 };
