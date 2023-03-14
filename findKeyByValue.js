702158a47e8aa749cde02d289b1f1c7fb23162f9
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-14 00:06:02 -0400

    refactored findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let statement = "";
-  if (actual === expected) {
-    statement = `Assertion Passed:  ${actual} === ${expected}`;
-  }
-  if (actual !== expected) {
-    statement = `Assertion Failed:  ${actual} !== ${expected}`;
-  }
-  return statement;
-};
-
 const findKeyByValue = function(searchThis, forThis) {
   let foundIt = null;
   for (const key in searchThis) {
