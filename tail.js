469e12d6b8353e5c0e24de7fac80002a56e5dd91
nelsonchengg <nelsoncheng103@gmail.com>
2023-03-13 20:46:24 -0400

    Refactor tail

--- tail.js
+++ tail.js
@@ -15,6 +15,4 @@ const tail = function(array) {
   return value;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
